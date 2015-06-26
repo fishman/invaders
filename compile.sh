3992865c3bb839191b3f659552f201df2a23dee3
Reza Jelveh <toyb0mb@gmx.net>
2015-06-26 11:52:39 -0700

    add elf header for vmware

--- compile.sh
+++ compile.sh
@@ -3,16 +3,18 @@
 set -e
 set -v
 
-CFLAGS="-fno-builtin -nostdinc -O2 -I. -Wall -Werror"
-LDFLAGS="-nostdlib -Wl,-N -Wl,-Ttext -Wl,100000"
-gcc $CFLAGS -c keyboard.c
-gcc $CFLAGS -c delay.c
-gcc $CFLAGS -c common.c
-gcc $CFLAGS -c sound.c
-gcc $CFLAGS -c game.c
-gcc $CFLAGS -c kernel.c
-gcc $CFLAGS -c video.c
-gcc $CFLAGS -c memory.c
-gcc $CFLAGS -c boot.S
-gcc $LDFLAGS -o invaders.exec boot.o kernel.o keyboard.o video.o game.o sound.o delay.o common.o memory.o
+CC=gcc-4.6
+CFLAGS="-m32 -fno-builtin -nostdinc -O2 -I. -Wall -Wno-error"
+LDFLAGS="-m32 -nostdlib -Wl,-N -Wl,-Ttext -Wl,100000"
+${CC} $CFLAGS -c keyboard.c
+${CC} $CFLAGS -c delay.c
+${CC} $CFLAGS -c common.c
+${CC} $CFLAGS -c sound.c
+${CC} $CFLAGS -c game.c
+${CC} $CFLAGS -c kernel.c
+${CC} $CFLAGS -c video.c
+${CC} $CFLAGS -c memory.c
+${CC} $CFLAGS -c boot.S
+${CC} $LDFLAGS -o invaders.exec boot.o kernel.o keyboard.o video.o game.o sound.o delay.o common.o memory.o
+# objcopy -O elf32-i386 invaders.exec invaders
 objcopy -O binary invaders.exec invaders
